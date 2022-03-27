74d409ad2eb86d2442c692641576a33afe0533b0
Jason Ward <jason.ward.actual@gmail.com>
2022-03-27 00:19:54 -0500

    Add project files.

---
+++ New_SandBox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
